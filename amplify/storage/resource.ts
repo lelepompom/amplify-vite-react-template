605a4ee8261d6c95b7e226da02a8bf78d0d9c149
Sandra Ortega <sandra.ortega@vodafone.com>
2024-07-01 14:02:30 +0200

    No access policies

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,15 +1,5 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'amplifyInvoicesReadSoft',
-    access: (allow) => ({
-        'invoices/{entity_id}/*': [
-            allow.guest.to(['read']),
-            allow.entity('identity').to(['read', 'write', 'delete'])
-        ],
-        'invoices/*': [
-            allow.authenticated.to(['read', 'write']),
-            allow.guest.to(['read', 'write'])
-        ],
-    })
+    name: 'amplifyInvoicesReadSoft'
 });
